612b7c92943751426dbc5060b8d7d365feaf6cfa
3Pa6pi9 <153427488+3Pa6pi9@users.noreply.github.com>
2026-04-26 16:39:57 +0300

    AdminClient.tsx

--- app/admin/AdminClient.tsx
+++ app/admin/AdminClient.tsx
@@ -1,5 +1,7 @@
 'use client';
 import { useState } from 'react';
+import { createClient } from '@supabase/supabase-js';
+
 
 export default function AdminClient() {
   const [productName, setProductName] = useState('');
